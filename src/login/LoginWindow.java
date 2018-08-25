0663cf67442bbbf5571e7ebc031ea8bf1707770b
Calvin <calvinkinateder1@yahoo.com>
2018-08-25 19:11:21 -0400

    fixed mac issue with spawning pwdfield in create account

--- src/login/LoginWindow.java
+++ src/login/LoginWindow.java
@@ -26,6 +26,7 @@ public class LoginWindow extends javax.swing.JFrame {
      */
     public LoginWindow() {
         initComponents();
+        panelSwitcher(1);
     }
     AccountManager mngr = new AccountManager();
     User currentusr;
