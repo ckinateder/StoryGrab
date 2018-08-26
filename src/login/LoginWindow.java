2bb06520a81a950bf0a00e2243bab3b8bf5b9570
calvin kinateder <calvi@DESKTOP-41JKQP7>
2018-08-25 23:07:07 -0400

    add logo

--- src/login/LoginWindow.java
+++ src/login/LoginWindow.java
@@ -5,7 +5,14 @@
  */
 package login;
 
+import static com.sun.javafx.scene.control.skin.Utils.getResource;
 import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 import keeptoo.Drag;
 
 /**
@@ -21,7 +28,7 @@ public class LoginWindow extends javax.swing.JFrame {
         initComponents();
     }
     AccountManager mngr = new AccountManager();
-    
+    User currentusr;
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -42,10 +49,12 @@ public class LoginWindow extends javax.swing.JFrame {
         jLabel5 = new javax.swing.JLabel();
         nouserwithname = new javax.swing.JLabel();
         jLabel10 = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
         MainPanel = new keeptoo.KGradientPanel();
         jLabel8 = new javax.swing.JLabel();
         jLabel6 = new javax.swing.JLabel();
         jLabel7 = new javax.swing.JLabel();
+        logoutbtn = new keeptoo.KButton();
         CreateAccountPanel = new keeptoo.KGradientPanel();
         jLabel11 = new javax.swing.JLabel();
         jLabel13 = new javax.swing.JLabel();
@@ -59,6 +68,7 @@ public class LoginWindow extends javax.swing.JFrame {
         finalcreate = new keeptoo.KButton();
         jLabel17 = new javax.swing.JLabel();
         jLabel18 = new javax.swing.JLabel();
+        jLabel9 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setLocation(new java.awt.Point(0, 0));
@@ -224,6 +234,12 @@ public class LoginWindow extends javax.swing.JFrame {
         jLabel10.setText("password");
         LoginPanel.add(jLabel10, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 300, 70, -1));
 
+        jLabel1.setFont(new java.awt.Font("Segoe UI Semibold", 0, 30)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/login/icons8-news-50.png"))); // NOI18N
+        jLabel1.setText(" StoryGrab");
+        LoginPanel.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 10, 230, 60));
+
         getContentPane().add(LoginPanel, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 870, 600));
 
         MainPanel.setBorder(javax.swing.BorderFactory.createEmptyBorder(0, 0, 0, 0));
@@ -264,11 +280,41 @@ public class LoginWindow extends javax.swing.JFrame {
         MainPanel.add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(830, 10, 30, 30));
 
         jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/login/icons8-male-user-50.png"))); // NOI18N
-        MainPanel.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 20, -1, -1));
+        MainPanel.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 60, -1, -1));
 
         jLabel7.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         jLabel7.setForeground(new java.awt.Color(255, 255, 255));
-        MainPanel.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 80, 220, 20));
+        MainPanel.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 80, 280, 20));
+
+        logoutbtn.setText("Logout");
+        logoutbtn.setFont(new java.awt.Font("Segoe UI", 0, 12)); // NOI18N
+        logoutbtn.setkAllowTab(false);
+        logoutbtn.setkBorderRadius(40);
+        logoutbtn.setkEndColor(new java.awt.Color(0, 204, 51));
+        logoutbtn.setkHoverEndColor(new java.awt.Color(78, 160, 143));
+        logoutbtn.setkHoverForeGround(new java.awt.Color(255, 255, 255));
+        logoutbtn.setkHoverStartColor(new java.awt.Color(63, 167, 89));
+        logoutbtn.setkIndicatorThickness(0);
+        logoutbtn.setkPressedColor(new java.awt.Color(167, 69, 199));
+        logoutbtn.setkSelectedColor(new java.awt.Color(163, 62, 167));
+        logoutbtn.setName(""); // NOI18N
+        logoutbtn.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                logoutbtnMouseClicked(evt);
+            }
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                logoutbtnMousePressed(evt);
+            }
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                logoutbtnMouseReleased(evt);
+            }
+        });
+        logoutbtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                logoutbtnActionPerformed(evt);
+            }
+        });
+        MainPanel.add(logoutbtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 550, 150, 40));
 
         getContentPane().add(MainPanel, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 870, 600));
 
@@ -351,7 +397,6 @@ public class LoginWindow extends javax.swing.JFrame {
         fullnamefield.setBackground(new Color(0,0,0,0));
         fullnamefield.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         fullnamefield.setForeground(new java.awt.Color(255, 255, 255));
-        fullnamefield.setToolTipText("username");
         fullnamefield.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 2, 0, new java.awt.Color(255, 255, 255)));
         fullnamefield.setCaretColor(new java.awt.Color(255, 255, 255));
         fullnamefield.setCursor(new java.awt.Cursor(java.awt.Cursor.TEXT_CURSOR));
@@ -436,11 +481,16 @@ public class LoginWindow extends javax.swing.JFrame {
         jLabel18.setText("username");
         CreateAccountPanel.add(jLabel18, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 300, 70, -1));
 
+        jLabel9.setFont(new java.awt.Font("Segoe UI Semibold", 0, 30)); // NOI18N
+        jLabel9.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel9.setIcon(new javax.swing.ImageIcon(getClass().getResource("/login/icons8-news-50.png"))); // NOI18N
+        jLabel9.setText(" StoryGrab");
+        CreateAccountPanel.add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 10, 230, 60));
+
         getContentPane().add(CreateAccountPanel, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 870, 600));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
     
     private void userfieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_userfieldActionPerformed
         // TODO add your handling code here:
@@ -661,6 +711,26 @@ public class LoginWindow extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_LoginPanelMouseReleased
 
+    private void logoutbtnMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_logoutbtnMouseClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_logoutbtnMouseClicked
+
+    private void logoutbtnMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_logoutbtnMousePressed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_logoutbtnMousePressed
+
+    private void logoutbtnMouseReleased(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_logoutbtnMouseReleased
+        // TODO add your handling code here:
+        logout();
+    }//GEN-LAST:event_logoutbtnMouseReleased
+
+    private void logoutbtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_logoutbtnActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_logoutbtnActionPerformed
+    /*
+    Custom code here
+    */
+    
     public void resetAllFields(){
         pwdfield.setText("");
         pwdfield2.setText("");
@@ -669,6 +739,7 @@ public class LoginWindow extends javax.swing.JFrame {
         fullnamefield.setText("");
         nouserwithname.setText("");
         jLabel15.setText("");
+        jLabel7.setText("");
     }
     public void panelSwitcher(int sw){
         resetAllFields();
@@ -692,8 +763,7 @@ public class LoginWindow extends javax.swing.JFrame {
         }
         
     }
-    public void login(boolean fromWhere){//true from create account
-        User currentusr;
+    public void login(boolean fromWhere){//true from create account        
         if(fromWhere){
             currentusr = mngr.users.get(mngr.users.size()-1);//get last made account
             //this is being called from create account page
@@ -710,6 +780,10 @@ public class LoginWindow extends javax.swing.JFrame {
             jLabel7.setText(currentusr.screenName);
             panelSwitcher(2);
         }
+    }
+    public void logout(){
+        currentusr = null;
+        panelSwitcher(1);
     }
     /**
      * @param args the command line arguments
@@ -738,7 +812,7 @@ public class LoginWindow extends javax.swing.JFrame {
         }
         //</editor-fold>
         //</editor-fold>
-
+            
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
@@ -755,6 +829,7 @@ public class LoginWindow extends javax.swing.JFrame {
     private keeptoo.KButton backfromca;
     private keeptoo.KButton finalcreate;
     private javax.swing.JTextField fullnamefield;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel11;
     private javax.swing.JLabel jLabel13;
@@ -770,7 +845,9 @@ public class LoginWindow extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
     private keeptoo.KButton loginbtn;
+    private keeptoo.KButton logoutbtn;
     private javax.swing.JLabel nouserwithname;
     private javax.swing.JPasswordField pwdfield;
     private javax.swing.JPasswordField pwdfield2;
