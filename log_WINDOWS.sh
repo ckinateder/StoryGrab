530d7c64e27237aa5b346f48c2eacd762960fee7
calvin kinateder <calvi@DESKTOP-41JKQP7>
2018-10-11 01:51:34 -0400

    update ia

--- log_WINDOWS.sh
+++ log_WINDOWS.sh
@@ -1,4 +1,4 @@
-echo "Task,Planned action,Planned outcome,Time estimated,Target completion date" > "logs/log_$(date -I).csv"
-git log --date=short --pretty=format:',Work on code,%s, ,%ad (%ar)' >> logs/"log_$(date -I).csv"
-echo "Task,Planned action,Planned outcome,Time estimated,Target completion date" > "ia/currentlog.csv"
-git log --date=short --pretty=format:',Work on code,%s, ,%ad (%ar)' >> "ia/currentlog.csv"
+echo "Task==Planned action==Planned outcome==Time estimated==Target completion date" > "logs/log_$(date -I).csv"
+git log --date=short --pretty=format:'==Work on code==%s== ==%ad (%ar)' >> logs/"log_$(date -I).csv"
+echo "Task==Planned action==Planned outcome==Time estimated==Target completion date" > "ia/currentlog.csv"
+git log --date=short --pretty=format:'==Work on code==%s== ==%ad (%ar)' >> "ia/currentlog.csv"
